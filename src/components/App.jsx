cdc2c459fd9546922da77a6399361c140804273d
rebecalasconi <rebecavoicilas@yahoo.com>
2024-10-08 08:08:45 +1400

    first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
+import React from 'react';
+import Profile from '../components/Profile/Profile';
+import user from '../components/Profile/user.json';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
-};
+}
